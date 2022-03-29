3dde2e50fbb4f6cbf4ea4d4ad44336b72db7a018
Deepak <saideepakreddy0308@gmail.com>
2022-03-29 12:45:17 +0530

    Added loading bar at top

--- src/components/News.js
+++ src/components/News.js
@@ -30,11 +30,15 @@ export class News extends Component {
     }
 
     async updateNews() {
+        this.props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b72b691e64c44ff18e0196caa73ed805&page=1&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(35);
         let parsedData = await data.json();
+        this.props.setProgress(70)
         this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false })
+        this.props.setProgress(100);
     }
 
     async componentDidMount() {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,8 +6,8 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card" >
-                    {/* style={{width: "20rem"}} */}
-                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}> {source} </span>
+                    <div style={{display:'flex', justifyContent:'flex-end',position:'absolute', right:'0'}}>
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '80%', zIndex: '1'}}> {source} </span></div>
                     <img src={!imageUrl ? "https://www.sciencenews.org/wp-content/uploads/2021/02/022421_mt_number-generator_feat.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
