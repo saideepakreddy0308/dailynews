e015e4111908c2799f9d42bbe9503d0aa23323ab
Deepak <saideepakreddy0308@gmail.com>
2022-03-29 10:46:32 +0530

    Added badge and more info

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let { title, description, imageUrl, newsUrl } = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="my-3">
                 <div className="card" >
                     {/* style={{width: "20rem"}} */}
+                    <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}> {source} </span>
                     <img src={!imageUrl ? "https://www.sciencenews.org/wp-content/uploads/2021/02/022421_mt_number-generator_feat.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toLocaleString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
