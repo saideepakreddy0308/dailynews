90997680b383f10f9baf92d7cc9b32fe4ffcaafc
MOON KNIGHT <90776053+saideepakreddy0308@users.noreply.github.com>
2022-04-28 01:04:56 +0530

    routes setup-countries

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Navbar from './components/Navbar';
 import News from "./components/News";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
-import Footer from "./components/Footer";
-
 export default class App extends Component {
   pageSize = 6;
   state = {
@@ -30,6 +28,59 @@ export default class App extends Component {
         <Route exact path="/sports"><News setProgress={this.setProgress}  key = "sports"  pageSize={this.pageSize} country="in" category="sports"/> </Route>
         <Route exact path="/technology"><News setProgress={this.setProgress}  key = "technology"  pageSize={this.pageSize} country="in" category="technology"/> </Route>
         <Route exact path="/us"><News setProgress={this.setProgress}  key = "us"  pageSize={this.pageSize} country="us" category="general"/> </Route>
+        <Route exact path="/gb"><News setProgress={this.setProgress}  key = "gb"  pageSize={this.pageSize} country="gb" category="general"/> </Route>
+        <Route exact path="/ae"><News setProgress={this.setProgress}  key = "ae"  pageSize={this.pageSize} country="ae" category="general"/> </Route>
+        <Route exact path="/ar"><News setProgress={this.setProgress}  key = "ar"  pageSize={this.pageSize} country="ar" category="general"/> </Route>
+        <Route exact path="/at"><News setProgress={this.setProgress}  key = "at"  pageSize={this.pageSize} country="at" category="general"/> </Route>
+        <Route exact path="/au"><News setProgress={this.setProgress}  key = "au"  pageSize={this.pageSize} country="au" category="general"/> </Route>
+        <Route exact path="/be"><News setProgress={this.setProgress}  key = "be"  pageSize={this.pageSize} country="be" category="general"/> </Route>
+        <Route exact path="/bg"><News setProgress={this.setProgress}  key = "bg"  pageSize={this.pageSize} country="bg" category="general"/> </Route>
+        <Route exact path="/br"><News setProgress={this.setProgress}  key = "br"  pageSize={this.pageSize} country="br" category="general"/> </Route>
+        <Route exact path="/ca"><News setProgress={this.setProgress}  key = "ca"  pageSize={this.pageSize} country="ca" category="general"/> </Route>
+        <Route exact path="/ch"><News setProgress={this.setProgress}  key = "ch"  pageSize={this.pageSize} country="ch" category="general"/> </Route>
+        <Route exact path="/cn"><News setProgress={this.setProgress}  key = "cn"  pageSize={this.pageSize} country="cn" category="general"/> </Route>
+        <Route exact path="/co"><News setProgress={this.setProgress}  key = "co"  pageSize={this.pageSize} country="co" category="general"/> </Route>
+        <Route exact path="/cu"><News setProgress={this.setProgress}  key = "cu"  pageSize={this.pageSize} country="cu" category="general"/> </Route>
+        <Route exact path="/cz"><News setProgress={this.setProgress}  key = "cz"  pageSize={this.pageSize} country="cz" category="general"/> </Route>
+        <Route exact path="/de"><News setProgress={this.setProgress}  key = "de"  pageSize={this.pageSize} country="de" category="general"/> </Route>
+        <Route exact path="/eg"><News setProgress={this.setProgress}  key = "eg"  pageSize={this.pageSize} country="eg" category="general"/> </Route>
+        <Route exact path="/fr"><News setProgress={this.setProgress}  key = "fr"  pageSize={this.pageSize} country="fr" category="general"/> </Route>
+        <Route exact path="/gr"><News setProgress={this.setProgress}  key = "gr"  pageSize={this.pageSize} country="gr" category="general"/> </Route>
+        <Route exact path="/hk"><News setProgress={this.setProgress}  key = "hk"  pageSize={this.pageSize} country="hk" category="general"/> </Route>
+        <Route exact path="/hu"><News setProgress={this.setProgress}  key = "hu"  pageSize={this.pageSize} country="hu" category="general"/> </Route>
+        <Route exact path="/id"><News setProgress={this.setProgress}  key = "id"  pageSize={this.pageSize} country="id" category="general"/> </Route>
+        <Route exact path="/ie"><News setProgress={this.setProgress}  key = "ie"  pageSize={this.pageSize} country="ie" category="general"/> </Route>
+        <Route exact path="/il"><News setProgress={this.setProgress}  key = "il"  pageSize={this.pageSize} country="il" category="general"/> </Route>
+        <Route exact path="/it"><News setProgress={this.setProgress}  key = "it"  pageSize={this.pageSize} country="it" category="general"/> </Route>
+        <Route exact path="/jp"><News setProgress={this.setProgress}  key = "jp"  pageSize={this.pageSize} country="jp" category="general"/> </Route>
+        <Route exact path="/kr"><News setProgress={this.setProgress}  key = "kr"  pageSize={this.pageSize} country="kr" category="general"/> </Route>
+        <Route exact path="/lt"><News setProgress={this.setProgress}  key = "lt"  pageSize={this.pageSize} country="lt" category="general"/> </Route>
+        <Route exact path="/lv"><News setProgress={this.setProgress}  key = "lv"  pageSize={this.pageSize} country="lv" category="general"/> </Route>
+        <Route exact path="/ma"><News setProgress={this.setProgress}  key = "ma"  pageSize={this.pageSize} country="ma" category="general"/> </Route>
+        <Route exact path="/mx"><News setProgress={this.setProgress}  key = "mx"  pageSize={this.pageSize} country="mx" category="general"/> </Route>
+        <Route exact path="/my"><News setProgress={this.setProgress}  key = "my"  pageSize={this.pageSize} country="my" category="general"/> </Route>
+        <Route exact path="/ng"><News setProgress={this.setProgress}  key = "ng"  pageSize={this.pageSize} country="ng" category="general"/> </Route>
+        <Route exact path="/nl"><News setProgress={this.setProgress}  key = "nl"  pageSize={this.pageSize} country="nl" category="general"/> </Route>
+        <Route exact path="/no"><News setProgress={this.setProgress}  key = "no"  pageSize={this.pageSize} country="no" category="general"/> </Route>
+        <Route exact path="/nz"><News setProgress={this.setProgress}  key = "nz"  pageSize={this.pageSize} country="nz" category="general"/> </Route>
+        <Route exact path="/ph"><News setProgress={this.setProgress}  key = "ph"  pageSize={this.pageSize} country="ph" category="general"/> </Route>
+        <Route exact path="/pl"><News setProgress={this.setProgress}  key = "pl"  pageSize={this.pageSize} country="pl" category="general"/> </Route>
+        <Route exact path="/pt"><News setProgress={this.setProgress}  key = "pt"  pageSize={this.pageSize} country="pt" category="general"/> </Route>
+        <Route exact path="/ro"><News setProgress={this.setProgress}  key = "ro"  pageSize={this.pageSize} country="ro" category="general"/> </Route>
+        <Route exact path="/rs"><News setProgress={this.setProgress}  key = "rs"  pageSize={this.pageSize} country="rs" category="general"/> </Route>
+        <Route exact path="/ru"><News setProgress={this.setProgress}  key = "ru"  pageSize={this.pageSize} country="ru" category="general"/> </Route>
+        <Route exact path="/sa"><News setProgress={this.setProgress}  key = "sa"  pageSize={this.pageSize} country="sa" category="general"/> </Route>
+        <Route exact path="/se"><News setProgress={this.setProgress}  key = "se"  pageSize={this.pageSize} country="se" category="general"/> </Route>
+        <Route exact path="/sg"><News setProgress={this.setProgress}  key = "sg"  pageSize={this.pageSize} country="sg" category="general"/> </Route>
+        <Route exact path="/si"><News setProgress={this.setProgress}  key = "si"  pageSize={this.pageSize} country="si" category="general"/> </Route>
+        <Route exact path="/sk"><News setProgress={this.setProgress}  key = "sk"  pageSize={this.pageSize} country="sk" category="general"/> </Route>
+        <Route exact path="/za"><News setProgress={this.setProgress}  key = "za"  pageSize={this.pageSize} country="za" category="general"/> </Route>
+        <Route exact path="/th"><News setProgress={this.setProgress}  key = "th"  pageSize={this.pageSize} country="th" category="general"/> </Route>
+        <Route exact path="/tr"><News setProgress={this.setProgress}  key = "tr"  pageSize={this.pageSize} country="tr" category="general"/> </Route>
+        <Route exact path="/tw"><News setProgress={this.setProgress}  key = "tw"  pageSize={this.pageSize} country="tw" category="general"/> </Route>
+        <Route exact path="/ua"><News setProgress={this.setProgress}  key = "ua"  pageSize={this.pageSize} country="ua" category="general"/> </Route>
+        <Route exact path="/ve"><News setProgress={this.setProgress}  key = "ve"  pageSize={this.pageSize} country="ve" category="general"/> </Route>
+        {/* <Route exact path="/in"><News setProgress={this.setProgress}  key = "in"  pageSize={this.pageSize} country="in" category="general"/> </Route> */}
         </Switch>
         {/* <Footer /> */}
         </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export class NavBar extends Component {
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-danger p-1">
                     <div className="container-fluid">
                         <img src="https://img.icons8.com/fluency/48/000000/news.png" style={{ height: "2rem", maxWidth: "50%" }} />
-                        <Link className="navbar-brand" to="/">DailyNews</Link>
+                        <Link className="navbar-brand" to="/">DailyNews <sup className='text-info'>IN</sup></Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -22,13 +22,75 @@ export class NavBar extends Component {
                                 <li className="nav-item"><Link className="nav-link" to="/technology">TECHNOLOGY</Link></li>
                                 <li className="nav-item"><Link className="nav-link" to="/entertainment">ENTERTAINMENT</Link></li>
                                 <li className="nav-item"><Link className="nav-link" to="/sports">SPORTS</Link></li>
+                                <li className="nav-item dropdown" >
+          <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Countries
+          </a>
+          <ul className="dropdown-menu bg-dark text-light" aria-labelledby="navbarDropdown" style={{ fontSize: 15 }}>
+            {/* <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="./in">India</Link></li> */}
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/us">United States</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/gb">United Kingdom</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ae">United Arab Emirates</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ar">Argentina</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/at">Austria</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/au">Australia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/be">Belgium</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/bg">Bulgaria</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/br">Brazil</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ca">Canada</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ch">Switzerland</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/cn">China</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/co">Colombia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/cu">Cuba</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/cz">Czech Republic</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/de">Germany</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/eg">Egypt</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/fr">France</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/gr">Greece</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/hk">Hong Kong</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/hu">Hungary</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/id">Indonesia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ie">Ireland</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/il">Israel</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/it">Italy</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/jp">Japan</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/kr">South Korea</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/lt">Lithuania</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/lv">Latvia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ma">Morocco</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/mx">Mexico</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/my">Malaysia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ng">Nigeria</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/nl">Netherlands</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/no">Norway</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/nz">New Zealand</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ph">Philippines</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/pl">Poland</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/pt">Portugal</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ro">Romonia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/rs">Serbia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ru">Russia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/sa">Saudi Arabia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/se">Sweden</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/sg">Singapore</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/si">Slovenia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/sk">Slovakia</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/za">South Africa</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/th">Thailand</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/tr">Turkey</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/tw">Taiwan</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ua">Ukraine</Link></li>
+            <li  className="dropdown-item " ><Link className="dropdown-link text-muted text-decoration-none" to="/ve">Venezuela</Link></li>
+
+          </ul>
+        </li>
                             </ul>
                         <form className="d-flex">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
                         </div>
-                           <a className='d-flex mx-1' href="https://github.com/saideepakreddy0308"> <img className="text-light" src="https://img.icons8.com/material-outlined/24/000000/github.png"/ ></a>
+                           {/* <a className='d-flex mx-1' href="https://github.com/saideepakreddy0308"> <img className="text-light" src="https://img.icons8.com/material-outlined/24/000000/github.png"/ ></a> */}
                     </div>
                 </nav>
             </div>
